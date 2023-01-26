07669a9298733837f81b7d7a0bf6f11c6cc59fa9
lapmu <antjdgkdl@gmail.com>
2023-01-26 19:48:23 +0900

    dummyData.js add

---
+++ data/dummyData.js
@@ -0,0 +1,18 @@
+const dummyData = {
+    "post": [
+        {
+            "id":1,
+            "title":"post title",
+            "body":"post body"
+        }
+    ],
+    "visit": [
+        {
+            "id":1,
+            "author":"moomin",
+            "title":"Hi"
+        }
+    ]
+}
+
+export default dummyData
