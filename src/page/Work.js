88a0d99ca40414cad852fca3657616f1894b7cf6
dding-v <v.dding.v@gmail.com>
2023-01-29 17:33:49 +0900

    work.js map 추가

--- src/page/Work.js
+++ src/page/Work.js
@@ -1,9 +1,13 @@
 import React from "react";
+import List from '../components/List'
 
-const Work = () => {
+const Work = ({ data }) => {
+
     return(
         <div>
-            it's Work
+            {data.map(datas => (
+                <List datas={datas} key={datas.id} />
+            ))}
         </div>
     )
 }
