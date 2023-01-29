ec1fbf1fcf3c83127f02adeceb7731102bdc2d79
lapmu <antjdgkdl@gmail.com>
2023-01-29 22:46:23 +0900

    guest constent css 정렬, 간격 수정, guest 더미 데이터 추가

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -1,25 +1,43 @@
 const dummyData = {
-    "post": [
-        {
-            "id":1,
-            "title":"post title",
-            "body":"post body"
-        }
-    ],
-    "guest": [
-        {
-            "id":1,
-            "author":"moomin",
-            "title":"Hi",
-            "img":'none'
-        },
-        {
-            "id":2,
-            "author":"minmoo",
-            "title":"bye",
-            "img":'none'
-        }
-    ]
-}
+  post: [
+    {
+      id: 1,
+      title: "post title",
+      body: "post body",
+    },
+  ],
+  guest: [
+    {
+      id: 1,
+      author: "moomin",
+      title: "Hi",
+      img: "none",
+    },
+    {
+      id: 2,
+      author: "minmoo",
+      title: "bye",
+      img: "none",
+    },
+    {
+      id: 3,
+      author: "moomin",
+      title: "Hi",
+      img: "none",
+    },
+    {
+      id: 4,
+      author: "moomin",
+      title: "Hi",
+      img: "none",
+    },
+    {
+      id: 5,
+      author: "moomin",
+      title: "Hi",
+      img: "none",
+    },
+  ],
+};
 
-export default dummyData
+export default dummyData;
