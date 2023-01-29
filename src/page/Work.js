581a9c25b029eb1723bc349078473d53d23ceb93
dding-v <v.dding.v@gmail.com>
2023-01-29 18:55:56 +0900

    Write.js 수정 / css 추가

--- src/page/Work.js
+++ src/page/Work.js
@@ -1,10 +1,12 @@
 import React from "react";
 import List from '../components/List'
+import Write from '../page/Write'
 
 const Work = ({ data }) => {
 
     return(
         <div>
+            <Write />
             {data.map(datas => (
                 <List datas={datas} key={datas.id} />
             ))}
