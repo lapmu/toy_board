724b293a6d4012cfbd6a55558391e91a1287ad11
dding-v <v.dding.v@gmail.com>
2023-01-30 19:04:48 +0900

    작업 전 push

--- src/page/Work.js
+++ src/page/Work.js
@@ -1,17 +1,14 @@
 import React from "react";
-import List from '../components/List'
-import Write from '../page/Write'
+import List from "../components/List";
 
 const Work = ({ data }) => {
-
-    return(
-        <div>
-            {data.map(datas => (
-                <List datas={datas} key={datas.id} />
-                ))}
-                {/* <Write /> */}
-        </div>
-    )
-}
+  return (
+    <div>
+      {data.map((datas) => (
+        <List datas={datas} key={datas.id} />
+      ))}
+    </div>
+  );
+};
 
 export default Work;
