093429ae5be5a0c8a1cd1129c3349a5680dc598a
dding-v <v.dding.v@gmail.com>
2023-02-04 22:55:33 +0900

    검색 인덱스 순서 구현

--- src/components/List.js
+++ src/components/List.js
@@ -4,27 +4,27 @@ import { Link, useNavigate } from "react-router-dom";
 import classes from "./List.module.css";
 
 const List = ({ data, search }) => {
-  // id, title, body, createAt
+  // id, title, body
+
+  const filtered = data.filter((el) => {
+    return search.toLowerCase() === ""
+      ? el
+      : el.title.toLowerCase().includes(search);
+  });
 
   return (
     <div>
-      {data
-        .filter((el) => {
-          return search.toLowerCase() === ""
-            ? el
-            : el.title.toLowerCase().includes(search);
-        })
-        .map((el, idx) => {
-          return (
-            <Link to={"/postitem" + idx} key={idx}>
-              <section className={classes.main}>
-                <div className={classes.idx}>{data.length - idx}</div>
-                <div className={classes.text}>{el.title}</div>
-                <div>{el.createAt}</div>
-              </section>
-            </Link>
-          );
-        })}
+      {filtered.map((el, idx) => {
+        return (
+          <Link to={"/postitem" + idx} key={idx}>
+            <section className={classes.main}>
+              <div className={classes.idx}>{filtered.length - idx}</div>
+              <div className={classes.text}>{el.title}</div>
+              <div>{el.createAt}</div>
+            </section>
+          </Link>
+        );
+      })}
     </div>
   );
 };
