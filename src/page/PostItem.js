0d2a978e21d55e1813421e8fa6e3ead904ae6e31
lapmu <antjdgkdl@gmail.com>
2023-02-04 15:05:16 +0900

    postItem data.body 수정

--- src/page/PostItem.js
+++ src/page/PostItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import classes from './PpstItem.module.css'
+import classes from "./PpstItem.module.css";
 
 const PostItem = ({ data }) => {
   return (
     <>
       <div>{data.title}</div>
-      <div>{data.body}</div>
+      <div dangerouslySetInnerHTML={{ __html: data.body }}></div>
       <div>{data.createAt}</div>
     </>
   );
