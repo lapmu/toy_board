83cec7c3fb6521ce7ff193e678a530715c00b77c
dding-v <v.dding.v@gmail.com>
2023-02-06 15:43:52 +0900

    main border css remove

--- src/page/Menu.js
+++ src/page/Menu.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const Menu = () => {
   return (
-    <div>menu
+    <div>
+        <div>
+            <div>Moo</div>
+            <div>Min</div>
+        </div>
+        <div>
+            <p> 다른 게시판 링크 걸기 </p>
+        </div>
     </div>
   );
 };
