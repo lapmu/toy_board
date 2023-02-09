4373466a57325b3c43ffcc51f5938f779c0372c7
dding-v <v.dding.v@gmail.com>
2023-02-09 20:22:29 +0900

    nav 수정

--- src/page/Main.js
+++ src/page/Main.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import classes from './Main.module.css'
 
+import logo from '../img/mnmz_logo.png'
+
 const Main = () => {
   return (
     <div className={classes.main}>
