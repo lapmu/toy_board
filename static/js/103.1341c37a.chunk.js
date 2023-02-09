ae98f94928eba871aff378c416b495a5506efc0d
lapmu <antjdgkdl@gmail.com>
2023-02-09 20:38:18 +0900

    Updates

---
+++ static/js/103.1341c37a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktoy_board=self.webpackChunktoy_board||[]).push([[103],{8103:function(n,a,e){e.r(a),e.d(a,{default:function(){return c}});e(2791);var i="Main_main__t-a2c",s="Main_title__vFlfx",t=(e(7194),e(184)),c=function(){return(0,t.jsxs)("div",{className:i,children:[(0,t.jsx)("div",{className:s,children:"M&MZ"}),(0,t.jsx)("div",{})]})}}}]);
+//# sourceMappingURL=103.1341c37a.chunk.js.map
--- static/js/103.efa6b3ec.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktoy_board=self.webpackChunktoy_board||[]).push([[103],{8103:function(n,a,e){e.r(a),e.d(a,{default:function(){return c}});e(2791);var i="Main_main__t-a2c",s="Main_title__vFlfx",t=e(184),c=function(){return(0,t.jsxs)("div",{className:i,children:[(0,t.jsx)("div",{className:s,children:"MMZ"}),(0,t.jsx)("div",{})]})}}}]);
-//# sourceMappingURL=103.efa6b3ec.chunk.js.map
