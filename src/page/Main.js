291e769d81a42414742df8534a4d9932f5e06f95
dding-v <v.dding.v@gmail.com>
2023-02-10 20:52:44 +0900

    main 명언

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-import classes from './Main.module.css'
+import classes from "./Main.module.css";
 
-import logo from '../img/mnmz_logo.png'
+import logo from "../img/mnmz_logo.png";
 
 const Main = () => {
   return (
     <div className={classes.main}>
-        <div className={classes.title}>M&MZ</div>
-        <div></div>
+      <div className={classes.title}>M&MZ</div>
+      <div className={classes.title_2}>
+        <span>When you have faults, do not fear to abandon them. – Confucius</span>
+      </div>
     </div>
   );
 };
